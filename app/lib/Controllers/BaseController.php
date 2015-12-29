dea2582a3faa1ef465fed83b0c005bd5f1698914
Bertrand Moulard <bmoulard@etsy.com>
2015-12-29 13:39:19 -0500

    Move library code to lib directory

--- app/lib/Controllers/BaseController.php
+++ app/lib/Controllers/BaseController.php
@@ -4,7 +4,7 @@ namespace Cooking\Controllers;
 
 class BaseController {
 
-  const TPL_BASE = __DIR__ . "/../templates/";
+  const TPL_BASE = __DIR__ . "/../../templates/";
 
   public function render($template, $data) {
     include self::TPL_BASE . "layout/application.php";
