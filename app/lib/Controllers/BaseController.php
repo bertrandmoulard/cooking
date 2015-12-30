712ae3a7f08c3fb161186ec236d15a8c45cd88d1
Bertrand Moulard <bmoulard@etsy.com>
2015-12-30 15:58:47 -0500

    Move generic controller methods to base controller

--- app/lib/Controllers/BaseController.php
+++ app/lib/Controllers/BaseController.php
@@ -2,11 +2,31 @@
 
 namespace Cooking\Controllers;
 
-class BaseController {
+abstract class BaseController {
 
-  const TPL_BASE = __DIR__ . "/../../templates/";
+    const TPL_BASE = __DIR__ . "/../../templates/";
 
-  public function render($template, $data) {
-    include self::TPL_BASE . "layout/application.php";
-  }
+    protected $resource;
+
+    public function render($template, $data) {
+        $template = "$this->resource/$template";
+        include self::TPL_BASE . "layout/application.php";
+    }
+
+    public function view($id) {
+        $mapper = $this->getMapperInstance();
+        $model = $mapper->find($id);
+        $this->render('view.php', $model);
+    }
+
+    public function index() {
+        $mapper = $this->getMapperInstance();
+        $models = $mapper->all();
+        $this->render('index.php', $models);
+    }
+
+    private function getMapperInstance() {
+        $class = 'Cooking\Mappers\\' . $this->resource . "Mapper";
+        return new $class();
+    }
 }
--- app/lib/Controllers/RecipesController.php
+++ app/lib/Controllers/RecipesController.php
@@ -2,21 +2,7 @@
 
 namespace Cooking\Controllers;
 
-use \Cooking\Models\Recipe;
-use \Cooking\Mappers\RecipeMapper;
-
 class RecipesController extends BaseController {
-
-    public function view($id) {
-        $mapper = new RecipeMapper();
-        $recipe = $mapper->find($id);
-        $this->render('recipes/view.php', $recipe);
-    }
-
-    public function index() {
-        $mapper = new RecipeMapper();
-        $recipes = $mapper->all();
-        $this->render('recipes/index.php', $recipes);
-    }
+    protected $resource = "recipe";
 }
 
