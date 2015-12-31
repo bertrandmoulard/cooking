f0f35361630d2e812e6befe2f4fee059d5d3039a
Bertrand Moulard <bmoulard@etsy.com>
2015-12-31 11:10:40 -0500

    Add css and javascript base files and redirect rules

--- app/templates/layout/application.php
+++ app/templates/layout/application.php
@@ -1,8 +1,12 @@
 <html>
+<head>
+<link rel="stylesheet" href="css/application.css" />
 <body>
 <!-- application layout -->
 <?php
+include __DIR__ . "/header.php";
 include __DIR__ . "/../$template";
 ?>
+<script src="js/application.js"></script>
 </body>
 </html>
