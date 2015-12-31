833deefff2d856947b8fa9423311108ca938505a
Bertrand Moulard <bmoulard@etsy.com>
2015-12-31 12:20:02 -0500

    Add phpunit

---
+++ tests/sampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+
+class SampleTest extends PHPUnit_Framework_TestCase {
+    public function testSample() {
+        $this->assertEquals(true, false);
+    }
+}
