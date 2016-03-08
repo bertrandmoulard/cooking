922cfb41b473afcc4f62456dbddb438b46833eff
Bertrand Moulard <bmoulard@etsy.com>
2016-03-08 12:23:24 -0500

    Remove unused method

--- app/lib/Mappers/RecipeMapper.php
+++ app/lib/Mappers/RecipeMapper.php
@@ -11,16 +11,4 @@ class RecipeMapper extends BaseMapper {
     protected function getTableName() {
         return 'recipes';
     }
-
-    public function allWithChef() {
-        $statement = $this->pdo->prepare("SELECT * FROM {$this->getTableName()} ORDER BY id DESC");
-        $statement->execute();
-        $results = $statement->fetchAll();
-        $models = [];
-        foreach($results as $result) {
-            array_push($models, $this->hydrateModel($result));
-        }
-        return $models;
-    }
-
 }
